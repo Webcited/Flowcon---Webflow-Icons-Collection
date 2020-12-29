1133ac731682f71e448cb0caf89b5b76ff5e0c9c
shivam <shivamgit@pm.me>
2020-12-29 16:05:58 +0530

    Fix error
    Signed-off-by: shivam <shivamgit@pm.me>

--- source/server/index.js
+++ source/server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const axios = require('axios');
-const FormData = require('form-data');
 const morgan = require('morgan');
 const process = require('process');
 
